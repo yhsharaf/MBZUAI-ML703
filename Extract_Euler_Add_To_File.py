241f72cc728f9d4bda186d5560e65a415331f3fc
yhsharaf <yousef.h.sharaf@gmail.com>
2023-04-15 06:59:42 +0400

    Bash File To Create Folders

--- Extract_Euler_Add_To_File.py
+++ Extract_Euler_Add_To_File.py
@@ -3,7 +3,7 @@ import os
 import glob
 
 # Set the directory containing the text files
-dir_path = '/home/yhsharaf/Desktop/MBZUAI-ML703/TestingWater'
+dir_path = '/home/yhsharaf/Desktop/MBZUAI-ML703/BIWISubset'
 
 # Get a list of all the text files in the directory
 txt_files = glob.glob(os.path.join(dir_path, '*.txt'))
@@ -20,18 +20,19 @@ for file_path in txt_files:
     for line in first_three_lines:
         row = [float(x) for x in line.split()]
         matrix.append(row)
-    
+
     # Convert the list of lists to a numpy array and print it
     matrix = np.array(matrix)
     # print(matrix)
     # Calculate pitch (rotation around X-axis)
-    pitch = np.arctan2(-matrix[2,1], np.sqrt(matrix[0,1]**2 + matrix[1,1]**2))
+    pitch = np.arctan2(-matrix[2, 1],
+                       np.sqrt(matrix[0, 1]**2 + matrix[1, 1]**2))
 
     # Calculate roll (rotation around Y-axis)
-    roll = np.arctan2(matrix[2,0], matrix[2,2])
+    roll = np.arctan2(matrix[2, 0], matrix[2, 2])
 
     # Calculate yaw (rotation around Z-axis)
-    yaw = np.arctan2(-matrix[1,0], matrix[0,0])
+    yaw = np.arctan2(-matrix[1, 0], matrix[0, 0])
 
     # Print the results in radians
     # print("Pitch: radians",np.radians(pitch))
@@ -44,9 +45,8 @@ for file_path in txt_files:
 
     pitch_roll_yaw = str(pitch) + ',' + str(roll) + ',' + str(yaw)
 
-    # print(pitch_roll_yaw) 
+    # print(pitch_roll_yaw)
     # Append the matrix to the end of the file
     with open(file_path, 'a') as file:
-        file.write('\n')
         file.write('XYZ: '+pitch_roll_yaw)
-
+        file.write('\n')
