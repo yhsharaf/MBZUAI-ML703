ee5809a97f6a01e30491ff2fa6c228b404c6b576
yhsharaf <yousef.h.sharaf@gmail.com>
2023-04-15 17:24:24 +0400

    Formatting Only

--- Extract_Euler_Demo.py
+++ Extract_Euler_Demo.py
@@ -1,21 +1,20 @@
 import numpy as np
 
 # Define the rotation matrix
-R = np.array([[0.997874, 0.0557298, 0.0338006 ],
-[-0.0304369 ,0.856994, -0.514426 ],
-[-0.0576358, 0.512304 ,0.856868 ]])
+R = np.array([[0.997874, 0.0557298, 0.0338006],
+              [-0.0304369, 0.856994, -0.514426],
+              [-0.0576358, 0.512304, 0.856868]])
 
 # Calculate pitch (rotation around X-axis)
-pitch = np.arctan2(-R[2,1], np.sqrt(R[0,1]**2 + R[1,1]**2))
+pitch = np.arctan2(-R[2, 1], np.sqrt(R[0, 1]**2 + R[1, 1]**2))
 
 # Calculate roll (rotation around Y-axis)
-roll = np.arctan2(R[2,0], R[2,2])
+roll = np.arctan2(R[2, 0], R[2, 2])
 
 # Calculate yaw (rotation around Z-axis)
-yaw = np.arctan2(-R[1,0], R[0,0])
+yaw = np.arctan2(-R[1, 0], R[0, 0])
 
 # Print the results in degrees
-print("Pitch:  deg",np.degrees(pitch))
-print("Roll:  deg",np.degrees(roll))
-print("Yaw:  deg",np.degrees(yaw))
-
+print("Pitch:  deg", np.degrees(pitch))
+print("Roll:  deg", np.degrees(roll))
+print("Yaw:  deg", np.degrees(yaw))
