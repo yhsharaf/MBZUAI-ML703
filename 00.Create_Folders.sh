67c2a35d2c94ab1064a838b19e9b57ec5e43b8a6
yhsharaf <yousef.h.sharaf@gmail.com>
2023-04-17 09:13:29 +0400

    Create Folders For All BIWIDataset

--- 00.Create_Folders.sh
+++ 00.Create_Folders.sh
@@ -2,6 +2,7 @@
 
 folder_name="Clusters"
 subfolders=("KMeans" "GMM" "Ignore")
+subsubfolders=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19" "20" "21" "22" "23" "24")
 values=("1" "3" "5" "7" "9" "11")
 
 if [ ! -d "$folder_name" ]; then
@@ -13,15 +14,22 @@ for subfolder in "${subfolders[@]}"; do
   subfolder_path="$folder_name/$subfolder"
   if [ ! -d "$subfolder_path" ]; then
     mkdir "$subfolder_path"
-    echo "New subfolder '$subfolder_path' created!"
+    # echo "New subfolder '$subfolder_path' created!"
   fi
-  
-  for value in "${values[@]}"; do
-    value_path="$subfolder_path/$value"
-    if [ ! -d "$value_path" ]; then
-      mkdir "$value_path"
-      echo "New subfolder '$value_path' created!"
+
+  for subsubfolder in "${subsubfolders[@]}"; do
+    subsubfolder_path="$subfolder_path/$subsubfolder"
+    if [ ! -d "$subsubfolder_path" ]; then
+      mkdir "$subsubfolder_path"
+      # echo "New subsubfolder '$subsubfolder_path' created!"
     fi
+  
+    for value in "${values[@]}"; do
+      value_path="$subsubfolder_path/$value"
+      if [ ! -d "$value_path" ]; then
+        mkdir "$value_path"
+        # echo "New value folder '$value_path' created!"
+      fi
+    done
   done
 done
-
